c9807d1af386452abee21014d6b8d7052260a7e6
Peter Schiansky <peter.schiansky@univie.ac.at>
2022-09-07 18:32:56 +0200

    make it a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name = 'thermotek',
+version = '0.1.0',
+author = 'Peter Schiansky',
+author_email = 'peter.schiansky@univie.ac.at',
+description = 'Interface to Thermotek Chillers',
+license='GPLv3',
+packages=['thermotek'],
+zip_safe=False)
---
+++ thermotek/__init__.py
@@ -0,0 +1,2 @@
+from .t255p import T255P
+from .t257p import T257P
